4d43b29518c8b20e75421afde76f5552ef0f383c
Ahmed Salama <ahmedslamaaa934@gamil.com>
2023-03-14 23:16:20 +0200

    TRY Vsc

--- Unit_2_C_Programming/C Basics/lab 1/main.c
+++ Unit_2_C_Programming/C Basics/lab 1/main.c
@@ -6,29 +6,31 @@
  */
 #include <stdio.h>
 #define pi 3.14
-int main (void){
-	char choice ;
-	float  raduis;
-	double result  ;
+int main(void)
+{
+	char choice;
+	float raduis;
+	double result;
 	printf("please enter radius of circle \n");
-	fflush(stdin); fflush(stdout);
-	scanf ("%f",&raduis );
-	printf (" please enter if area enter a and if circumference of circle  enter c ");
-	fflush(stdin); fflush(stdout);
-	scanf ("%c",&choice );
-	if (choice =='a'){
-		result = raduis * raduis *pi;
-		printf("the area  of  circle =%lF",result);
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%f", &raduis);
+	printf(" please enter if area enter a and if circumference of circle  enter c ");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%c", &choice);
+	if (choice == 'a')
+	{
+		result = raduis * raduis * pi;
+		printf("the area  of  circle =%lF", result);
 	}
-	else if (choice=='c'){
-		result =2*raduis *pi;
-		printf("the circumference  of  circle =%lF",result);
+	else if (choice == 'c')
+	{
+		result = 2 * raduis * pi;
+		printf("the circumference  of  circle =%lF", result);
 	}
 	else
 		printf("error");
 
-
-
 	return 0;
 }
-
